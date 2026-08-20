f3b6f5b460a12cde89643d5eeb69d494ee59a3ae
Michele Addamiano <addamianom@gmail.com>
2026-08-20 16:55:19 +0000

    init

---
+++ zmayrap2_fr/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("com.esempirap.zmayrap2fr.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
